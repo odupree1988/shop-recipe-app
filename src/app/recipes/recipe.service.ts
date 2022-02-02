f021f31f0c3efa4a1c9b6d1ebc791d54f54f6be2
Owen Dupree <odupree1988@gmail.com>
2022-02-02 16:54:06 -0500

    first commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,26 @@
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  recipeSeleted = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else is there to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}

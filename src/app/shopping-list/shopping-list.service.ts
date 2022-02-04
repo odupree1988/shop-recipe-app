c9d3cbb7c08403a67fd9dfb272cb70d4676b3d61
Owen Dupree <odupree1988@gmail.com>
2022-02-04 16:08:54 -0500

    added routes to recipes and shopping-list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -15,22 +15,22 @@ export class ShoppingListService {
   }
 
   addIngredient(ingredient: Ingredient) {
-    let exists: boolean = false;
-    let that = this;
+    // let exists: boolean = false;
+    // let that = this;
 
-    let newIngr = parseInt(Object.entries(ingredient)[1][1]);
+    // console.log(parseInt(Object.entries(ingredient)[1][1]));
 
-    that.ingredients.forEach((ingr, key) => {
-      if (ingr['name'] == ingredient['name']) {
-        ingr['amount'] += newIngr;
-        exists = true;
-      }
-      if (!exists) {
-      }
-    });
-    that.ingredients.push(ingredient);
-    console.log(that.ingredients);
-    // this.ingredients.push(ingredient);
+    // that.ingredients.forEach((ingr, key) => {
+    //   if (ingr['name'] == ingredient['name']) {
+    //     ingr['amount'] += ingredient['amount'];
+    //     exists = true;
+    //   }
+    //   if (!exists) {
+    //     that.ingredients.push(ingredient);
+    //     console.log(that.ingredients);
+    //   }
+    // });
+    this.ingredients.push(ingredient);
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
 
@@ -39,27 +39,26 @@ export class ShoppingListService {
     // for (let ingredient of ingredients) {
     //   this.addIngredient(ingredient);
     // }
-    let exist: boolean = false;
-    let that = this;
-    //debugger;
-    console.log(this.ingredients);
-    ingredients.forEach(function (newIngr, newKey) {
-      exist = false;
-      console.log(newIngr);
-      that.ingredients.forEach(function (ingr, key) {
-        if (ingr['name'] == newIngr['name']) {
-          ingr['amount'] += newIngr['amount'];
-          exist = true;
-        }
-      });
-      if (!exist) {
-        that.ingredients.push(newIngr);
-        console.log(that.ingredients);
-      }
-    });
-    // this.ingredientsChanged.emit(this.ingredients.slice());
-    console.log(this.ingredients.slice());
-    // this.ingredients.push(...ingredients);
+    // let exist: boolean = false;
+    // let that = this;
+    // //debugger;
+    // console.log(this.ingredients);
+    // ingredients.forEach(function (newIngr, newKey) {
+    //   exist = false;
+    //   console.log(newIngr);
+    //   that.ingredients.forEach(function (ingr, key) {
+    //     if (ingr['name'] == newIngr['name']) {
+    //       ingr['amount'] += newIngr['amount'];
+    //       exist = true;
+    //     }
+    //   });
+    //   if (!exist) {
+    //     return newing
+    //     console.log(that.ingredients);
+    //   }
+    // });
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
     //write logic to check and see if item has been added already and add onto the item if it has been
   }
 }
